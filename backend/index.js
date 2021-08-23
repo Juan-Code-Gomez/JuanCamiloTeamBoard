49d673f43a757b5e59068bf47fde6d12df8fe2cb
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-08-23 15:39:41 -0500

    Se agrega el schema de role y se hacen pruebas guardandolo en la base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {dbConnection} = require("./db/db");
 require("dotenv").config();
+const Role = require("./routers/role");
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/role", Role);
 
 
 app.listen(process.env.PORT,()=>{
